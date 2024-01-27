4c51a947d4b7e50c84e3a6fa8d37b597fe8a7608
Malú <130713333+malummartiins@users.noreply.github.com>
2024-01-27 18:00:48 -0300

    update

--- cursoJSarray/testes.js
+++ cursoJSarray/testes.js
@@ -24,7 +24,7 @@ function dobraNumero(num){
 
 const listaNumeros = [1,2,3];
 const listDois = executaOperacaoEmArray(listaNumeros,dobraNumero)
-console.log(listDois);*/
+console.log(listDois);
 
 const array = [1,2,3,4,5,6];
 const numProcurado = 4;
@@ -37,3 +37,58 @@ for (i = 0; i < array.length ; i++){
     }
 }
 console.log(`O NUMERO PROCURADO É ${numProcurado} O INDICE É ${POSI}`);
+
+
+Utilize o método concat() para unir os arrays das turmas A e B 
+em um único array chamado todasAsTurmas. Depois, utilize o método find() 
+para buscar um aluno específico pelo nome no array todosAlunos. 
+Exiba no console uma mensagem informando o nome do aluno procurado; 
+caso não exista na lista, retorne uma mensagem de aviso, por exemplo 
+Aluno não encontrado.
+
+const nomesTurmaA = [
+    'João Silva',
+    'Maria Santos',
+    'Pedro Almeida'
+  ];
+  
+  const nomesTurmaB = [
+    'Carlos Oliveira',
+    'Ana Souza',
+    'Lucas Fernandes'
+  ];
+
+  const todasAsTurmas = nomesTurmaA.concat(nomesTurmaB);
+
+  const alunoProcurado = todasAsTurmas.find(aluno => aluno === 'Ana Souza');
+
+
+if (alunoProcurado){
+    console.log( `Aluno encontrado: ${alunoProcurado}`);
+}else{
+    console.log('Aluno não encontrado');
+}
+*/
+
+/*Utilize o método forEach() para multiplicar cada elemento do 
+array por 3 e exibir o resultado de cada multiplicação. Depois, 
+utilize o método findIndex() para encontrar o índice do número 18 no array.*/
+
+
+const numeros = [6, 9, 12, 15, 18, 21];
+
+numeros.forEach(numero => {
+    const resultado = numero * 3;
+    console.log(resultado);
+})
+
+const index18 = numeros.findIndex(numero => numero === 18);
+
+if (indiceDoNumero18 !== -1) {
+    console.log(`O número 18 está no índice ${indiceDoNumero18}.`);
+  } else {
+    console.log('O número 18 não está presente no array.');
+  }
+
+
+
